ae60c6329365b807f0b5badb028c9913b616001d
girishsapkale <i.girishsapkale.com>
2016-08-09 11:35:59 +0530

    Files added

---
+++ begin_ruby.rb
@@ -0,0 +1,18 @@
+
+
+puts "This is main Ruby Program"
+
+END {
+   puts "Terminating Ruby Program"
+}
+BEGIN {
+   puts "Initializing Ruby Program"
+}
+
+=begin
+This is a comment.
+This is a comment, too.
+This is a comment, too.
+I said that already.
+=end
+
